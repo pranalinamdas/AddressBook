a81ba804c15e9086e4a9853278ce3471978542af
pranalikisannamdas <pranalinamdas96@gmail.com>
2019-09-26 22:37:36 +0530

    Add. two people into address book returns two people

--- src/test/java/com/thoughtworks/AddressBookTest.java
+++ src/test/java/com/thoughtworks/AddressBookTest.java
@@ -35,4 +35,18 @@ public class AddressBookTest {
         assertEquals(1, addressBook.get().size());
     }
 
+    @Test
+    void givenTwoPeopleAdded_WhenRetrieveAddressBookContents_ThenShouldReturnTwoPeople(){
+        Address address = new Address("Maharashtra", "Pune", 412202);
+        Person pranali = new Person("pranali", 98877676, address);
+
+        Address address1 = new Address("Maharashtra", "Nashik", 432402);
+        Person apeksha = new Person("apeksha", 87509676, address1);
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addPerson(pranali);
+        addressBook.addPerson(apeksha);
+
+        assertEquals(2, addressBook.get().size());
+    }
 }
